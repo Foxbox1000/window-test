c59aaaf37dbe73aaf2de6514c752d1626d9970a9
J Chehrazi <105824619+Foxbox1000@users.noreply.github.com>
2023-01-27 15:06:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-let spencer = window.open("https://maps.triangledayschool.com/spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0,alwaysOnTop=true,fullscreen=yes');
+let spencer = window.open("https://google.com/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0,alwaysOnTop=true,fullscreen=yes');
