f2dc6a2b5ee8296b2462deb6bd7e68bcdd406c65
J Chehrazi <105824619+Foxbox1000@users.noreply.github.com>
2023-01-27 09:40:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-let spencer = window.open("https://maps.triangledayschool.com/spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0,alwaysOnTop=true');
-
-setInterval(function() {
-  if (localStorage.getItem("activated") === "false") {
-    window.close();
-  } else if (spencer.closed) {
-    spencer = window.open("https://maps.triangledayschool.com/spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0,alwaysOnTop=true');
-    if (document.hidden) {
-      setTimeout(window.close, 10);
-    };
-  } else if (!window.opener) {
-    window.open("https://maps.triangledayschool.com/map/");
-    setTimeout(window.close(), 10);
-    setTimeout(spencer.close(), 5);
-  };
-}, 50);
+let spencer = window.open("https://maps.triangledayschool.com/spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0,alwaysOnTop=true,fullscreen=yes');
