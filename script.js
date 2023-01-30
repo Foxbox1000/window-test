9d27ff67e7945cada3ed6ae2c2db4e6fe32afd9f
J Chehrazi <105824619+Foxbox1000@users.noreply.github.com>
2023-01-30 13:55:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 addEventListener("click", function() {
   var popup = window.open("https://foxbox1000.github.io/window-test/window/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysOnTop=true,fullscreen=yes');
-  popup.document.documentElement.requestFullscreen();
+  setTimeout(function() {
+    popup.document.documentElement.requestFullscreen();
+  }, 2000);
 });
