13b24b71b1a77b516b69d6dae4a9818582633bed
J Chehrazi <105824619+Foxbox1000@users.noreply.github.com>
2023-01-30 14:45:46 -0800

    Update script.js

--- window/script.js
+++ window/script.js
@@ -1,1 +1,9 @@
-document.requestFullscreen()
+var elem = document.documentElement;
+setInterval(function() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
+    elem.mozRequestFullScreen();
+  }
+}, 500);
