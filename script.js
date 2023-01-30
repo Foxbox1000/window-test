42050ec54717bfb13375c560ac22025c817c523a
J Chehrazi <105824619+Foxbox1000@users.noreply.github.com>
2023-01-30 13:36:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-let spencer = window.open("https://foxbox1000.github.io/window-test/window/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0,alwaysOnTop=true,fullscreen="yes"');
+var popup = window.open("https://foxbox1000.github.io/window-test/window/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysOnTop=true,fullscreen=yes');
+popup.document.documentElement.requestFullscreen();
